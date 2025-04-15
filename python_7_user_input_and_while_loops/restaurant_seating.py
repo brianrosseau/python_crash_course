3bb29721df7db44e7dd5e4f008a6e3db08f65610
brianrosseau <138725169+brianrosseau@users.noreply.github.com>
2025-04-15 07:36:39 -0400

    Delete python_7_user_input_and_while_loops directory

--- python_7_user_input_and_while_loops/restaurant_seating.py
+++
@@ -1,8 +0,0 @@
-restaurant_seating = input("Welcome to Lorie's Lovely Cafe!\nHow many in your party this evening? ")
-restaurant_seating = int(restaurant_seating)
-
-if restaurant_seating > 8:
-    print("Please wait as we get a table ready for you.")
-else:
-    print("Your table is ready!")
-
