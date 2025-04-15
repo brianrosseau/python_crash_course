e4e89842bfd5316f756f780b638cb87693a84787
brianrosseau <138725169+brianrosseau@users.noreply.github.com>
2025-04-15 07:35:41 -0400

    Delete python_11_testing_your_code directory

--- python_11_testing_your_code/exercises/city_functions.py
+++
@@ -1,4 +0,0 @@
-def get_city_country(city, country):
-    """Generate a neatly formatted 'City, Country' name."""
-    city_country = f"{city}, {country}"
-    return city_country.title()
--- python_11_testing_your_code/exercises/population_functions.py
+++
@@ -1,9 +0,0 @@
-# Here I simply modified the get_city_country() function from city_functions.py to include population
-
-def get_city_country(city, country, population=''):
-    """Generate a neatly formatted 'City, Country - population xxx' name."""
-    if population:
-        city_country = f"{city}, {country} - population {population}"
-    else:
-        city_country = f"{city}, {country}"
-    return city_country.title()
