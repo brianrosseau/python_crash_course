8b5393b09909e139708a2b9e31465df90abe9838
brianrosseau <138725169+brianrosseau@users.noreply.github.com>
2025-04-14 20:23:19 -0400

    Delete ch_15_projects/python_4_working_with_lists directory

--- ch_15_projects/python_4_working_with_lists/one_million.py
+++
@@ -1,9 +0,0 @@
-one_million = list(range(1, 1000001))
-for number in one_million:
-    print(number)
-
-one_thousand = list(range(1001))
-for number in one_thousand:
-    print(number)
-
-    
--- ch_15_projects/python_4_working_with_lists/summing_a_million.py
+++
@@ -1,4 +0,0 @@
-one_million = list(range(1, 1000001))
-min(one_million)
-max(one_million)
-sum(one_million)
