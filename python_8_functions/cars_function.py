cee4beddb063add346f58a52d01e3f500b7dba1a
brianrosseau <138725169+brianrosseau@users.noreply.github.com>
2025-04-15 07:36:48 -0400

    Delete python_8_functions directory

--- python_8_functions/cars_function.py
+++
@@ -1,5 +0,0 @@
-def make_car(manufacturer, model, **car_info):
-    """Build a dictionary containing everything we know about a car."""
-    car_info['make'] = manufacturer
-    car_info['model'] = model
-    return car_info
--- python_8_functions/message.py
+++
@@ -1,5 +0,0 @@
-def display_message():
-    """Description of what I'm learning in chapter 8 of Python Crash Course"""
-    print("In this chapter I am learning about functions.")
-
-display_message()
